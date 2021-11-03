8a343bcfc7aa1a238f4c1b59277f11a13fbac506
pikcars <44181174+pikcars@users.noreply.github.com>
2021-11-03 01:05:47 +0100

    new

---
+++ database/config.php
@@ -0,0 +1,28 @@
+<?php 
+   class Database{
+       private $host = 'localhost';
+       private $port = 3306;
+       private $db_name = 'academyapp';
+       private $username = 'root';
+       private $password = '';
+       private $conn;
+
+       public function connect(){
+        $this->conn= null;
+
+
+        try {
+           $this->conn=new PDO('mysql:host=' .$this->host. ';port='.$this->port.';dbname='. $this->db_name,
+           $this->username,$this->password);
+           $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       } catch(PDOException $e){
+           echo 'Connection Error:'.$e->getMessage();
+       }
+       return $this->conn;
+   }
+ 
+
+
+
+
+   }
